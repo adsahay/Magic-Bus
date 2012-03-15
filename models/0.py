a2ee17c93983ce4c5608dedbd6554bdf0cbbb0f7
Aditya Sahay <aditya.sahay@gmail.com>
2012-03-15 12:22:53 +0530

    First commit

---
+++ models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'Magic Bus'
+settings.subtitle = 'Last.fm + YouTube mashup'
+settings.author = 'adsahay'
+settings.author_email = 'adi@sahay.co'
+settings.keywords = ''
+settings.description = 'Have a tiny concert play on your browser with Magic Bus.'
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '0a578f81-e544-439b-ae52-65e2e78925ba'
+settings.email_server = 'localhost'
+settings.email_sender = ''
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
